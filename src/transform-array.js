ac7f69b6237f4c1d7d60ceb23b3c7a1113ea2b18
Dmitry Grishin <57833466+GeraltDmitry@users.noreply.github.com>
2020-11-01 21:42:18 +0300

    fetch: fetch

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,21 @@
-const CustomError = require("../extensions/custom-error");
+  
+module.exports = function transform(array) {
+    result = [];
+    if (!Array.isArray(array)) throw Exception("The argument is not an array");
+    for (let i = 0; i < array.length; i++){
+            switch(array[i]) {
+                case "--discard-prev" : if(i>0) result.pop();
+                    break;
+                case "--discard-next" : i++;
+                    break;
+                case "--double-prev" : if(i>0) result.push(array[i-1]);
+                    break;
+                case "--double-next" : if(i+1<array.length) result.push(array[i+1]);
+                    break;
+                default: result.push(array[i]);
+                    break;
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+        }
+    }
+    return result;
 };
